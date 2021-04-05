1f8d6a76166184dcfac989dfce482da3f2b1f4d8
Julien Vanharen <julien.vanharen@gmail.com>
2021-04-05 18:58:27 +0200

    Initial commit

--- src/components/alert.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Text } from 'spectacle';
-
-export default class Alert extends React.Component {
-    render() { return (<Text {...this.props} color="red" style={{ display: 'inline' }}>{this.props.children}</Text>) }
-}
--- src/components/info.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Text } from 'spectacle';
-
-export default class Info extends React.Component {
-    render() { return (<Text color="blue" style={{ display: 'inline' }}>{this.props.children}</Text>) }
-}
